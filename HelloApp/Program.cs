3ad02cd9b6176e6bff65ae6bbfe31633fd113eca
Anand R <anandr72@gmail.com>
2018-09-07 08:24:26 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Enter Key to Exit.");
+
+            Console.ReadLine();
+        }
+    }
+}
